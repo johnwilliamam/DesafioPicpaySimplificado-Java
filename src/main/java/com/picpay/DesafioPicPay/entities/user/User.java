0e7f9bd0b24cd31d90aa694ede6bbb37b10ad005
John William <j,williamd.a.m@outlook.com>
2023-08-23 08:51:22 -0300

    Feat: create UserEntity

--- src/main/java/com/picpay/DesafioPicPay/entities/user/User.java
+++ src/main/java/com/picpay/DesafioPicPay/entities/user/User.java
@@ -1,10 +1,8 @@
 package com.picpay.DesafioPicPay.entities.user;
 
+import com.picpay.DesafioPicPay.dtos.UserDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -13,6 +11,7 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class User {
     @Id
@@ -28,4 +27,14 @@ public class User {
     private BigDecimal balance;
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    public User(UserDTO user) {
+        this.firstName = user.firstName();
+        this.lastName = user.lastName();
+        this.balance = user.balance();
+        this.document = user.document();
+        this.email = user.email();
+        this.password = user.password();
+        this.userType = user.type();
+    }
 }
