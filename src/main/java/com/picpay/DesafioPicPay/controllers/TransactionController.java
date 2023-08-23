10b512ec3bf5993e247855e7661a43f0cc0b7177
John William <j,williamd.a.m@outlook.com>
2023-08-23 08:52:15 -0300

    Feat: create TransactionController

---
+++ src/main/java/com/picpay/DesafioPicPay/controllers/TransactionController.java
@@ -0,0 +1,26 @@
+package com.picpay.DesafioPicPay.controllers;
+
+import com.picpay.DesafioPicPay.dtos.TransactionDTO;
+import com.picpay.DesafioPicPay.entities.transaction.Transaction;
+import com.picpay.DesafioPicPay.services.TransactionService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/transaction")
+public class TransactionController {
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO transaction) throws Exception {
+        Transaction newTransaction = this.transactionService.createTransaction(transaction);
+        return new ResponseEntity<>(newTransaction, HttpStatus.OK);
+    }
+}
